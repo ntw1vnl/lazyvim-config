ca30cfbf015f7c10d27e0342368ae96ddd3f8ca6
ntw1 <ntw1@mailo.com>
2025-11-01 13:10:48 +0100

    Added some keymap bindings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,6 +5,15 @@
 local keymap = vim.keymap
 
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+keymap.set("n", "x", '"_x', { desc = "Delete single character without copying into register" })
+
+-- Vertical scroll and center
+keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true, silent = true })
+keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true, silent = true })
+
+-- Find and center
+keymap.set("n", "n", "nzzzv")
+keymap.set("n", "N", "Nzzzv")
 
 keymap.set("n", "<leader>y", function()
   vim.fn.setreg(vim.v.register, vim.fn.expand("%:p"))
